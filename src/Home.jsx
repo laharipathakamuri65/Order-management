3907c6583b74c7ccf1a90fa6a10173c67d00cd07
laharipathakamuri65 <laharipathakamuri65@gmail.com>
2025-08-04 07:27:59 +0530

    Home page comment added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+//this is home page component
+// it will be used to display the home page content
+// or a welcome message in the application
 export default function Home() {
   return <h2>Home Page </h2>;
 }
